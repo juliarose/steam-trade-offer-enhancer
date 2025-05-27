18d023341bc41ba044bb1666b936a9ecfd4eda36
juliarose <julialunarose@gmail.com>
2025-05-27 15:57:03 -0400

    Build

--- steam.trade.offer.enhancer.user.js
+++ steam.trade.offer.enhancer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Steam Trade Offer Enhancer
 // @description Browser script to enhance Steam trade offers.
-// @version     2.2.4
+// @version     2.2.5
 // @author      Julia
 // @namespace   http://steamcommunity.com/profiles/76561198080179568/
 // @updateURL   https://github.com/juliarose/steam-trade-offer-enhancer/raw/master/steam.trade.offer.enhancer.meta.js
@@ -3600,7 +3600,7 @@ const scripts = [
 // These are shared between page scripts.
 const DEPS = (function() {
     // current version number of script
-    const VERSION = '2.2.4';
+    const VERSION = '2.2.5';
     // our window object for accessing globals
     const WINDOW = unsafeWindow;
     // dependencies to provide to each page script
